ef8dcb0db66f5b0ffbe5085265342ba81118a190
Fabio Berger <me@fabioberger.com>
2017-03-28 17:06:21 +0200

    Update ETH to WETH

--- ts/utils/constants.ts
+++ ts/utils/constants.ts
@@ -4,9 +4,9 @@ export const constants = {
     MIST_DOWNLOAD_URL: 'https://github.com/ethereum/mist/releases',
     NULL_ADDRESS: '0x0000000000000000000000000000000000000000',
     iconUrlBySymbol: {
-        ETH: '/images/token_icons/ether.png',
         TA: '/images/token_icons/clams.png',
         TB: '/images/token_icons/digixdao.png',
+        WETH: '/images/token_icons/ether.png',
         // FBTC: '/images/token_icons/btc.png',
         // FDGX: '/images/token_icons/digixdao.png',
     },
