5c24fce574298da7be2489ad8de85b08cfec2901
Fabio Berger <me@fabioberger.com>
2017-03-30 15:08:14 +0200

    Refactor out visualOrder component from signTransaction

--- ts/components/generate_order_flow/sign_transaction.tsx
+++ ts/components/generate_order_flow/sign_transaction.tsx
@@ -4,9 +4,8 @@ import {utils} from 'ts/utils/utils';
 import {Ox} from 'ts/utils/Ox';
 import {Blockchain} from 'ts/blockchain';
 import {Step} from 'ts/components/ui/step';
-import {Direction, SideToAssetToken, Side, AssetToken, HashData} from 'ts/types';
-import jazzicon = require('jazzicon');
-import ReactTooltip = require('react-tooltip');
+import {Direction, SideToAssetToken, HashData} from 'ts/types';
+import {VisualOrder} from 'ts/components/visual_order';
 
 const PRECISION = 5;
 
@@ -43,9 +42,6 @@ export class SignTransaction extends React.Component<SignTransactionProps, SignT
         };
     }
     public render() {
-        const depositAssetToken = this.props.sideToAssetToken[Side.deposit];
-        const receiveAssetToken = this.props.sideToAssetToken[Side.receive];
-        const expiryDate = utils.convertToReadableDateTimeFromUnixTimestamp(this.props.orderExpiryTimestamp);
         return (
             <Step
                 title="Confirm and sign your order"
@@ -54,75 +50,14 @@ export class SignTransaction extends React.Component<SignTransactionProps, SignT
                 hasBackButton={true}
                 onNavigateClick={this.onNavigationClickAsync.bind(this)}
             >
-                <div className="clearfix pt3">
-                    <div className="col col-5 center">
-                        {this.renderParty('Maker (you)', this.props.orderMakerAddress)}
-                    </div>
-                    <div className="col col-2 center">
-                        {this.renderAmount(depositAssetToken)}
-                        <div>
-                            <i
-                                style={{fontSize: 60, transform: 'rotate(180deg)'}}
-                                className="material-icons"
-                            >
-                                keyboard_return
-                            </i>
-                        </div>
-                        <div>
-                            <i style={{fontSize: 60}} className="material-icons">keyboard_return</i>
-                        </div>
-                        {this.renderAmount(receiveAssetToken)}
-                    </div>
-                    <div className="col col-5 center">
-                        {this.renderParty('Taker', this.props.orderTakerAddress)}
-                    </div>
-                </div>
-                <div className="center pt3 pb2">
-                    Expires: {expiryDate} UTC
-                </div>
+                <VisualOrder
+                    orderExpiryTimestamp={this.props.orderExpiryTimestamp}
+                    orderTakerAddress={this.props.orderTakerAddress}
+                    orderMakerAddress={this.props.orderMakerAddress}
+                    sideToAssetToken={this.props.sideToAssetToken}
+                />
                 <div>{this.state.isSigning && 'Signing...'}</div>
             </Step>
-        );
-    }
-    private renderAmount(assetToken: AssetToken) {
-        return (
-            <div style={{fontSize: 13}}>
-                {assetToken.amount.toFixed(PRECISION)} {assetToken.symbol}
-            </div>
-        );
-    }
-    private renderParty(party: string, address: string) {
-        const tooltipId = `${party}Tooltip`;
-        return (
-            <div>
-                <div className="pb1">{party}</div>
-                {this.renderIdenticon(address)}
-                <div
-                    className="mx-auto pt1"
-                    style={styles.address}
-                    data-tip={true}
-                    data-for={tooltipId}
-                >
-                    {!_.isEmpty(address) ? address : 'Anybody'}
-                </div>
-                {!_.isEmpty(address) && <ReactTooltip id={tooltipId}>{address}</ReactTooltip>}
-            </div>
-        );
-    }
-    private renderIdenticon(address: string) {
-        const diameter = 100;
-        const numericalAddress = this.convertAddressToNumber(address);
-        const jazzIcon = jazzicon(diameter, numericalAddress);
-        const innerHtml: string = jazzIcon.innerHTML;
-        return (
-            <div
-                className="circle mx-auto relative"
-                style={{width: diameter, height: diameter, overflow: 'hidden'}}
-            >
-                <div
-                    dangerouslySetInnerHTML={{__html: innerHtml}}
-                />
-            </div>
         );
     }
     private async onNavigationClickAsync(direction: Direction) {
@@ -163,10 +98,5 @@ export class SignTransaction extends React.Component<SignTransactionProps, SignT
         } else {
             this.props.updateGenerateOrderStep(direction);
         }
-    }
-    private convertAddressToNumber(address: string): number {
-        const addressWithoutPrefix = address.slice(2, 10);
-        const numericanAddress = parseInt(addressWithoutPrefix, 16);
-        return numericanAddress;
     }
 }
---
+++ ts/components/visual_order.tsx
@@ -0,0 +1,112 @@
+import * as _ from 'lodash';
+import * as React from 'react';
+import {utils} from 'ts/utils/utils';
+import {Ox} from 'ts/utils/Ox';
+import {Step} from 'ts/components/ui/step';
+import {Direction, SideToAssetToken, Side, AssetToken} from 'ts/types';
+import jazzicon = require('jazzicon');
+import ReactTooltip = require('react-tooltip');
+
+const PRECISION = 5;
+
+const styles = {
+    address: {
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+        width: '90px',
+    },
+};
+
+interface VisualOrderProps {
+    orderExpiryTimestamp: number;
+    orderTakerAddress: string;
+    orderMakerAddress: string;
+    sideToAssetToken: SideToAssetToken;
+}
+
+interface VisualOrderState {}
+
+export class VisualOrder extends React.Component<VisualOrderProps, VisualOrderState> {
+    public render() {
+        const depositAssetToken = this.props.sideToAssetToken[Side.deposit];
+        const receiveAssetToken = this.props.sideToAssetToken[Side.receive];
+        const expiryDate = utils.convertToReadableDateTimeFromUnixTimestamp(this.props.orderExpiryTimestamp);
+        return (
+            <div>
+                <div className="clearfix pt3">
+                    <div className="col col-5 center">
+                        {this.renderParty('Maker', this.props.orderMakerAddress)}
+                    </div>
+                    <div className="col col-2 center">
+                        {this.renderAmount(depositAssetToken)}
+                        <div>
+                            <i
+                                style={{fontSize: 60, transform: 'rotate(180deg)'}}
+                                className="material-icons"
+                            >
+                                keyboard_return
+                            </i>
+                        </div>
+                        <div>
+                            <i style={{fontSize: 60}} className="material-icons">keyboard_return</i>
+                        </div>
+                        {this.renderAmount(receiveAssetToken)}
+                    </div>
+                    <div className="col col-5 center">
+                        {this.renderParty('Taker', this.props.orderTakerAddress)}
+                    </div>
+                </div>
+                <div className="center pt3 pb2">
+                    Expires: {expiryDate} UTC
+                </div>
+            </div>
+        );
+    }
+    private renderAmount(assetToken: AssetToken) {
+        return (
+            <div style={{fontSize: 13}}>
+                {assetToken.amount.toFixed(PRECISION)} {assetToken.symbol}
+            </div>
+        );
+    }
+    private renderParty(party: string, address: string) {
+        const tooltipId = `${party}Tooltip`;
+        return (
+            <div>
+                <div className="pb1">{party}</div>
+                {this.renderIdenticon(address)}
+                <div
+                    className="mx-auto pt1"
+                    style={styles.address}
+                    data-tip={true}
+                    data-for={tooltipId}
+                >
+                    {!_.isEmpty(address) ? address : 'Anybody'}
+                </div>
+                {!_.isEmpty(address) && <ReactTooltip id={tooltipId}>{address}</ReactTooltip>}
+            </div>
+        );
+    }
+    private renderIdenticon(address: string) {
+        const diameter = 100;
+        const numericalAddress = this.convertAddressToNumber(address);
+        const jazzIcon = jazzicon(diameter, numericalAddress);
+        const innerHtml: string = jazzIcon.innerHTML;
+        return (
+            <div
+                className="circle mx-auto relative"
+                style={{width: diameter, height: diameter, overflow: 'hidden'}}
+            >
+                <div
+                    dangerouslySetInnerHTML={{__html: innerHtml}}
+                />
+            </div>
+        );
+    }
+    private convertAddressToNumber(address: string): number {
+        const addressWithoutPrefix = address.slice(2, 10);
+        const numericanAddress = parseInt(addressWithoutPrefix, 16);
+        return numericanAddress;
+    }
+}
