cb884a51a2ec291e83870331930b89529447b28e
Fabio Berger <me@fabioberger.com>
2017-04-06 11:51:46 +0200

    Implement an errorReporter that reports errors to Rollbar

---
+++ ts/utils/error_reporter.ts
@@ -0,0 +1,32 @@
+import {utils} from 'ts/utils/utils';
+import {constants} from 'ts/utils/constants';
+
+// Suggested way to include Rollbar with Webpack
+// https://github.com/rollbar/rollbar.js/tree/master/examples/webpack
+const rollbarConfig = {
+    accessToken: constants.ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    maxItems: 500,
+    payload: {
+        environment: constants.ENVIRONMENT,
+    },
+    uncaughtErrorLevel: 'error',
+};
+import Rollbar = require('../../public/js/rollbar.umd.nojson.min.js');
+const rollbar = Rollbar.init(rollbarConfig);
+
+export const errorReporter = {
+    reportAsync(err: Error): Promise<any> {
+        return new Promise((resolve, reject) => {
+            rollbar.error(err, (rollbarErr: Error) => {
+                if (rollbarErr) {
+                    utils.consoleLog(`Error reporting to rollbar, ignoring: ${rollbarErr}`);
+                    reject(rollbarErr);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    },
+};
