d391624ebaac5ef54f18a2eb317fa1792ff69a83
Fabio Berger <me@fabioberger.com>
2017-04-14 14:18:41 +0200

    Refactor label with styling into it's own component

--- ts/components/inputs/identicon_address_input.tsx
+++ ts/components/inputs/identicon_address_input.tsx
@@ -5,6 +5,7 @@ import {Blockchain} from 'ts/blockchain';
 import {Identicon} from 'ts/components/ui/identicon';
 import {RequiredLabel} from 'ts/components/ui/required_label';
 import {OrderAddressInput} from 'ts/components/inputs/order_address_input';
+import {InputLabel} from 'ts/components/ui/input_label';
 
 interface IdenticonAddressInputProps {
     blockchain: Blockchain;
@@ -16,31 +17,13 @@ interface IdenticonAddressInputProps {
 
 interface IdenticonAddressInputState {}
 
-const styles = {
-    label: {
-        color: colors.grey500,
-        left: 0,
-        marginTop: -6,
-        pointerEvents: 'none',
-        position: 'absolute',
-        textAlign: 'left',
-        top: 32,
-        transform: 'scale(0.75) translate(0px, -28px)',
-        transformOrigin: 'left top 0px',
-        transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
-        userSelect: 'none',
-        width: 240,
-        zIndex: 1,
-    },
-};
-
 export class IdenticonAddressInput extends React.Component<IdenticonAddressInputProps, IdenticonAddressInputState> {
     public render() {
         const label = this.props.isRequired ? <RequiredLabel label={this.props.label} /> :
                       this.props.label;
         return (
             <div className="relative">
-                <label style={styles.label}>{label}</label>
+                <InputLabel text={label} />
                 <div className="flex" style={{width: 385}}>
                     <div className="col col-1 pb1 pr1" style={{paddingTop: 27}}>
                         <Identicon address={this.props.address} diameter={25} />
--- ts/components/ui/fake_text_field.tsx
+++ ts/components/ui/fake_text_field.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {colors} from 'material-ui/styles';
+import {InputLabel} from 'ts/components/ui/input_label';
 
 const styles = {
     hr: {
@@ -13,21 +14,6 @@ const styles = {
         position: 'absolute',
         width: '100%',
     },
-    label: {
-        color: colors.grey500,
-        left: 0,
-        marginTop: -6,
-        pointerEvents: 'none',
-        position: 'absolute',
-        textAlign: 'left',
-        top: 32,
-        transform: 'scale(0.75) translate(0px, -28px)',
-        transformOrigin: 'left top 0px',
-        transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
-        userSelect: 'none',
-        width: 240,
-        zIndex: 1,
-    },
 };
 
 interface FakeTextFieldProps {
@@ -38,7 +24,7 @@ interface FakeTextFieldProps {
 export function FakeTextField(props: FakeTextFieldProps) {
     return (
         <div className="relative">
-            {props.label !== '' && <label style={styles.label}>{props.label}</label>}
+            {props.label !== '' && <InputLabel text={props.label} />}
             <div className="py2">
                 {props.children}
             </div>
