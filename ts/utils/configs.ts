f39be78a9abb01531e8bf652d4e91a119cc3440b
Fabio Berger <me@fabioberger.com>
2017-05-05 20:51:31 +0200

    Automatically turn on demo for development and staging, and off for live site

--- ts/utils/configs.ts
+++ ts/utils/configs.ts
@@ -1,13 +1,16 @@
+import * as _ from 'lodash';
 import {ProviderTypes} from 'ts/types';
 
 const BASE_URL = window.location.origin;
 
+const isDevelopment = _.includes(BASE_URL, 'http://0xproject.dev:8080') ||
+                      _.includes(BASE_URL, 'http://localhost:8080') ||
+                      _.includes(BASE_URL, 'http://127.0.0.1');
+const isStaging = _.includes(BASE_URL, 'staging-0xproject.s3-website-us-east-1.amazonaws.com');
+
 export const configs = {
     BASE_URL,
-    ENVIRONMENT: BASE_URL === 'http://0xproject.dev:8080' ||
-                 BASE_URL === 'http://localhost:8080' ?
-                 'development' :
-                 'production',
+    ENVIRONMENT: isDevelopment ? 'development' : 'production',
     PROVIDER_CONFIGS: {
         [ProviderTypes.publicNode]: {
             canSendTransactions: false,
@@ -17,5 +20,5 @@ export const configs = {
         },
     },
     symbolsOfMintableTokens: ['MKR', 'MLN', 'WETH', 'GNT', 'DGD', 'REP', 'ZRX'],
-    isDemoEnabled: true,
+    isDemoEnabled: isDevelopment || isStaging,
 };
