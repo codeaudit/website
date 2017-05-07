d012f6cd0cea62f9ad8ea6546dcf5ccb6f2488fe
Fabio Berger <me@fabioberger.com>
2017-05-07 16:51:07 +0200

    Update to https faucet url

--- ts/utils/constants.ts
+++ ts/utils/constants.ts
@@ -3,7 +3,7 @@ export const constants = {
     BITLY_ACCESS_TOKEN: 'ffc4c1a31e5143848fb7c523b39f91b9b213d208',
     BITLY_ENDPOINT: 'https://api-ssl.bitly.com',
     DEFAULT_TOKEN_ICON_URL: '/images/token_icons/default.png',
-    ETHER_FAUCET_ENDPOINT: 'http://174.138.84.92/rain',
+    ETHER_FAUCET_ENDPOINT: 'http://faucet.0xproject.com/rain',
     FEE_RECIPIENT_ADDRESS: '0x0000000000000000000000000000000000000000',
     INFURA_TESTNET_URL: 'https://kovan.infura.io/T5WSC8cautR4KXyYgsRs',
     MAKER_FEE: '0',
