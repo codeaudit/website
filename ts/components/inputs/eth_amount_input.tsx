760072fbf1eb247f8864a5ea013b7f69ee016cf2
Leonid Logvinov <logvinov.leon@gmail.com>
2017-05-15 10:04:31 +0200

    Add eth_amount_input

---
+++ ts/components/inputs/eth_amount_input.tsx
@@ -0,0 +1,46 @@
+import * as BigNumber from 'bignumber.js';
+import * as _ from 'lodash';
+import * as React from 'React';
+import {FailableBigNumberCallback, InputErrMsg} from 'ts/types';
+import {BalanceBoundedInput} from 'ts/components/inputs/balance_bounded_input';
+import {zeroEx} from 'ts/utils/zero_ex';
+import {constants} from 'ts/utils/constants';
+
+interface EthAmountInputProps {
+    label: string;
+    balance: BigNumber;
+    amount?: BigNumber;
+    onChange: FailableBigNumberCallback;
+    shouldShowIncompleteErrs: boolean;
+}
+
+interface EthAmountInputState {}
+
+export class EthAmountInput extends React.Component<EthAmountInputProps, EthAmountInputState> {
+    public render() {
+        const amount = this.props.amount ?
+            zeroEx.toUnitAmount(this.props.amount, constants.ETH_DECIMAL_PLACES) :
+            undefined;
+        return (
+            <div className="flex overflow-hidden" style={{height: 84}}>
+                <BalanceBoundedInput
+                    label={this.props.label}
+                    balance={this.props.balance}
+                    amount={amount}
+                    onChange={this.onChange.bind(this)}
+                    shouldCheckBalance={true}
+                    shouldShowIncompleteErrs={this.props.shouldShowIncompleteErrs}
+                />
+                <div style={{paddingTop: 44}}>
+                    ETH
+                </div>
+            </div>
+        );
+    }
+    private onChange(amount?: BigNumber) {
+        if (!_.isUndefined(amount)) {
+            amount = zeroEx.toBaseUnitAmount(amount, constants.ETH_DECIMAL_PLACES);
+        }
+        this.props.onChange(amount);
+    }
+}
