d3a94602d0fc4a92fadea6e99954c4b2b902abd3
Fabio Berger <me@fabioberger.com>
2017-05-19 17:33:58 +0200

    revert commit 5e044ac42f9985c01e92f8379da5f66d58003814

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const PRODUCTION = process.env.NODE_ENV === 'production';
-
 module.exports = {
     entry: ['./ts/index.tsx'],
     output: {
         path: path.join(__dirname, '/public'),
         filename: 'bundle.js',
     },
-    devtool: PRODUCTION ? 'cheap-module-source-map' : 'source-map',
+    devtool: 'source-map',
     resolve: {
         modules: [
             path.join(__dirname, '/ts'),
@@ -53,13 +51,4 @@ module.exports = {
       },
       disableHostCheck: true,
     },
-    plugins: PRODUCTION ?
-        [
-            new webpack.DefinePlugin({
-                'process.env': {
-                    'NODE_ENV': JSON.stringify('production'),
-                },
-            }),
-            new webpack.optimize.AggressiveMergingPlugin(),
-        ] : [],
 };
